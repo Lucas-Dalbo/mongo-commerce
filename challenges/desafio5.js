ec9780203e7580259363e5a7acdaa9e9e439e937
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-17 15:10:27 -0300

    feat: 04 to 06

---
+++ challenges/desafio5.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: 36 },
+      { vendidos: 85 },
+    ],
+  }, {
+    nome: true,
+    vendidos: true,
+    curtidas: true,
+    _id: false,    
+  },
+);
